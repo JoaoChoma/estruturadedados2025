ce0575e9e2fc81a07f79de841bc4b6992bb15d20
João Choma Neto <jchomax@gmail.com>
2025-04-02 20:39:03 -0300

    semana 01

---
+++ SEMANA01/helloword/soma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a, b, soma;
+    printf("Digite dois números: ");
+    scanf("%d %d", &a, &b);
+    soma = a + b;
+    printf("A soma é: %d\n", soma);
+    return 0;
+}
